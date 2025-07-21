23b45be39e213df17a1fc68431d96bbe3649b82c
kuwa <bcc32.kuwabara.c@gmail.com>
2025-07-21 11:43:39 +0900

    チェック項目を修正

--- CheckEnvironmentVariables.ps1
+++ CheckEnvironmentVariables.ps1
@@ -3,38 +3,26 @@ $projectIdentifier = "MD2025"
 
 # Environment variable keys
 $projectRootKey = "${projectIdentifier}_ROOT"
-$fixedResourcesKey = "${projectIdentifier}_FRES"
-$unityFixedResourcesKey = "${projectIdentifier}_UFRES"
-$unityResourcesKey = "${projectIdentifier}_URES"
-$unityScriptsKey = "${projectIdentifier}_USCRIPT"
+$dairyMemoKey = "${projectIdentifier}_DAIRY"
+$toolsKey = "${projectIdentifier}_TOOLS"
 
 # Get environment variables
 $projectRootPath = [System.Environment]::GetEnvironmentVariable($projectRootKey, [System.EnvironmentVariableTarget]::User)
-$fixedResourcesPath = [System.Environment]::GetEnvironmentVariable($fixedResourcesKey, [System.EnvironmentVariableTarget]::User)
-$unityFixedResourcesPath = [System.Environment]::GetEnvironmentVariable($unityFixedResourcesKey, [System.EnvironmentVariableTarget]::User)
-$unityResourcesPath = [System.Environment]::GetEnvironmentVariable($unityResourcesKey, [System.EnvironmentVariableTarget]::User)
-$unityScriptsPath = [System.Environment]::GetEnvironmentVariable($unityScriptsKey, [System.EnvironmentVariableTarget]::User)
+$dairyMemoPath = [System.Environment]::GetEnvironmentVariable($dairyMemoKey, [System.EnvironmentVariableTarget]::User)
+$toolsPath = [System.Environment]::GetEnvironmentVariable($toolsKey, [System.EnvironmentVariableTarget]::User)
 
 # Display environment variables
 Write-Host "Environment variable '$projectRootKey': $projectRootPath"
-Write-Host "Environment variable '$fixedResourcesKey': $fixedResourcesPath"
-Write-Host "Environment variable '$unityFixedResourcesKey': $unityFixedResourcesPath"
-Write-Host "Environment variable '$unityResourcesKey': $unityResourcesPath"
-Write-Host "Environment variable '$unityScriptsKey': $unityScriptsPath"
+Write-Host "Environment variable '$dairyMemoKey': $dairyMemoPath"
+Write-Host "Environment variable '$toolsKey': $toolsPath"
 
 # Check if environment variables are set
 if (-not $projectRootPath) {
     Write-Error "ERROR: Environment variable '$projectRootKey' is not set."
 }
-if (-not $fixedResourcesPath) {
-    Write-Error "ERROR: Environment variable '$fixedResourcesKey' is not set."
+if (-not $dairyMemoPath) {
+    Write-Error "ERROR: Environment variable '$dairyMemoKey' is not set."
 }
-if (-not $unityFixedResourcesPath) {
-    Write-Error "ERROR: Environment variable '$unityFixedResourcesKey' is not set."
-}
-if (-not $unityResourcesPath) {
-    Write-Error "ERROR: Environment variable '$unityResourcesKey' is not set."
-}
-if (-not $unityScriptsPath) {
-    Write-Error "ERROR: Environment variable '$unityScriptsKey' is not set."
+if (-not $toolsPath) {
+    Write-Error "ERROR: Environment variable '$toolsKey' is not set."
 }
